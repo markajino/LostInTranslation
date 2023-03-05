92479540cd8caa063a7572e21131034fc1502d82
Markozanis <46632994+markajino@users.noreply.github.com>
2023-03-05 17:52:32 +0500

    updated backend url

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,4 +1,4 @@
-export const API_ROOT = "https://trainer-api-production.up.railway.app";
+export const API_ROOT = "https://json-server-assingment-api-production.up.railway.app";
 export const API_KEY =
   "p7iaNxd+p7drw7A31r04+KH5m2l7hkB900CSxgD/xA6ryyszffO4gbHugEy5EbEOIwpymvZd9QyQnHCMU32CeA==";
 
